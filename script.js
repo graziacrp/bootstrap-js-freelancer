194fd43d8329fea2584418f0251d71828185b10f
graziacrp <graziacarpineta25@gmail.com>
2022-11-18 12:38:54 +0100

    aggiustando

--- script.js
+++ script.js
@@ -23,4 +23,48 @@ let ore = document.getElementById("Hours requested").value;
     
     
     let valoreSconto = controlloSconto("Sconti");
+
+
+
+
+    if (valoreSconto == 0 && discountVal!= ""){
+        alert("Codice sconto non valido.");
+        document.getElementById("discount").value = "Codice sconto non valido ";
+    }
+
+    let valore , valoreTotale;
+    switch (lavoro) {
+        case "Backend Development":
+            valore = (ore * costosviluppoBackEnd);
+            valoreTotale = valore - (valore * valoreSconto);
+            break;
+        case "Frontend Development": 
+            valore = ore * costosviluppoFrontEnd;
+            valoreTotale = valore - (valore * valoreSconto);
+            break;
+        case "Project Analisys":
+            valore = ore * costoprojectAnalist;
+            valoreTotale = valore - (valore * valoreSconto);
+            break;
+      
+    }
+    
+    valoreTotale = valoreTotale.toFixed(2);
+        
+    document.getElementById("Totale").innerHTML = "Il totale è : " + valoreTotale + " " + "\u20AC";
+    
+    
+
+    function controlloSconto(lista) { 
+        let discount = document.getElementById("discount").value; //codice sconto
+        discount = discount.toUpperCase();
+        let valoreSconto = 0;
+        for (let i = 0; i < lista.length; i++){
+            if (discount == lista[i]) { 
+                valoreSconto = 0.25;
+                break;
+            } 
+        }
+        return valoreSconto;
+    }
     
