0422363c94869bf4845d83bd09ec2eb64c281534
graziacrp <graziacarpineta25@gmail.com>
2022-11-18 12:50:09 +0100

    -

--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 //Calcolo i vari sconti in relazione alle ore
 
 function calcoloTotale() {
-
-    let costosviluppoBackEnd = 20.5;
-    let costosviluppoFrontEnd = 15.3;
-    let costoprojectAnalist = 33.6;
+    const costosviluppoBackEnd = 20.5;
+    const costosviluppoFrontEnd = 15.3;
+    const costoprojectAnalist = 33.6;
 }
 
 let Sconti = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
-alert("Sconti");
+
 
 let ore = document.getElementById("Hours requested").value;
     ore = parseInt(ore);
